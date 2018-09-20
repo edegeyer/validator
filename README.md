05e70448c1f095fcb6304bca7ec6ea21a3062cc2
Administrator <Administrator@139.20.163.207>
2018-09-20 14:19:58 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DSGVO_Validator
-txt Datei bitte mit Excel oeffen
+txt Datei bitte mit Excel oeffen, beim Importieren habe ich als leerzeichen als tausendertrennzeichen gewaehlt
+
+Die Excel Ausgabe fuer swap3and4 gibt an, dass sigma length und sigma distribution spielt gar keine Rolle.
+Das macht sinn weil wenn man dritte und vierte Character vertaeuscht, die Laenge dieser Zeichenkette aendert gar nicht, deshalbt theta length is null. Das gilt auch fuer theta distribution.
+
+ich lade bald die domainname zusammenfassung datei hoch 
+
+
--- src/main/java/com/masterarbeit/compare/StringComp.java
+++ src/main/java/com/masterarbeit/compare/StringComp.java
@@ -266,7 +266,12 @@ public class StringComp implements ComparerInterface {
     private double compareContent(String a, String b, double sig) {
 
         double order = compareOrder(a, b, Sigma.stro_);
+        
         double distribution = compareDistribution(a, b,Sigma.strd_);
+        System.out.println ("Sigma.strd_ is " +Sigma.strd_);
+        System.out.println ("theta  distribution  is " +distribution );
+        System.out.println ("Sigma.stro_ is " +Sigma.stro_);	
+        System.out.println ("theta  order  is " +order );	
         double result = 0.5 * (order + distribution);
     //    System.out.println("compareContent: " + result);
         return result;
@@ -356,10 +361,11 @@ public class StringComp implements ComparerInterface {
        // else
       //      System.out.println("String ist alpha " + _a);
 
-
-        double content = compareContent(_a, _b, sig);
         double length = compareLength(_a, _b, Sigma.strl_);
-
+        System.out.println ("Sigma.strl_ is " +Sigma.strl_);
+        System.out.println ("Theta length is " +length);
+        double content = compareContent(_a, _b, sig);
+       
         if (_a.toLowerCase().contains(_b.toLowerCase())||_b.toLowerCase().contains(_a.toLowerCase())){
             content = 0.0;
         }
